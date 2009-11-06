6e7fef50dd331ce527635a26057b4cf6b234d6a2
Steve Streza <stevestreza@gmail.com>
2009-11-05 23:16:28 -0500

    Added style sheet class

---
+++ TTStyleSheet.h
@@ -0,0 +1,26 @@
+//
+//  TTStyleSheet.h
+//  TTStyleMac
+//
+//  Created by Steve Streza on 11/5/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+extern const NSString *TTStyleSheetChangedNotification;
+
+@class TTStyle;
+
+@interface TTStyleSheet : NSObject {
+	NSMutableDictionary* _styles;
+}
+
++ (TTStyleSheet*)globalStyleSheet;
++ (void)setGlobalStyleSheet:(TTStyleSheet*)styleSheet;
+
+- (TTStyle*)styleWithSelector:(NSString*)selector;
+
+- (void)freeMemory;
+
+@end
---
+++ TTStyleSheet.m
@@ -0,0 +1,81 @@
+//
+//  TTStyleSheet.m
+//  TTStyleMac
+//
+//  Created by Steve Streza on 11/5/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "TTStyleSheet.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// global
+
+static TTStyleSheet* gStyleSheet = nil;
+const NSString *TTStyleSheetChangedNotification = @"TTStyleSheetChangedNotification";
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+@implementation TTStyleSheet
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// class public
+
++ (TTStyleSheet*)globalStyleSheet {
+/*
+ TODO Implement a default style sheet for Aqua
+ Perhaps make alternatives for other common UI styles
+ 
+	if (!gStyleSheet) {
+		gStyleSheet = [[TTDefaultStyleSheet alloc] init];
+	}
+ */
+	return gStyleSheet;
+}
+
++ (void)setGlobalStyleSheet:(TTStyleSheet*)styleSheet {
+	[gStyleSheet autorelease];
+	gStyleSheet = [styleSheet retain];
+	
+	[[NSNotificationCenter defaultCenter] postNotificationName: TTStyleSheetChangedNotification
+														object: gStyleSheet ];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// NSObject
+
+- (id)init {
+	if (self = [super init]) {
+		_styles = nil;
+	}
+	return self;
+}
+
+- (void)dealloc {
+	[_styles release];
+	_styles = nil;
+	
+	[super dealloc];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// public
+
+- (TTStyle*)styleWithSelector:(NSString*)selector {
+	TTStyle* style = [_styles objectForKey:selector];
+	if (!style) {
+		SEL sel = NSSelectorFromString(selector);
+		if ([self respondsToSelector:sel]) {
+			style = [self performSelector:sel withObject:nil];
+			if (style) {
+				if (!_styles) {
+					_styles = [[NSMutableDictionary alloc] init];
+				}
+				[_styles setObject:style forKey:selector];
+			}
+		}
+	}
+	return style;
+}
+
+@end
